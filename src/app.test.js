ce8cac6775a21fee5f13fdf462d44a3cb668413d
Palm •-• <ch.enderpalm@gmail.com>
2025-04-22 15:49:00 +0700

    fix: expected result in app.test.js

--- src/app.test.js
+++ src/app.test.js
@@ -3,10 +3,10 @@ const { dayOfTheWeek, monthOfTheYear } = require('./app');
 
 test('getDay returns the long-format day of the week', () => {
     const day = dayOfTheWeek( new Date('3/11/2020') );
-    expect( day ).toBe('Wednesday');
+    expect( day ).toBe('Tuesday');
 });
 
 test('getDay returns the long-format day of the week', () => {
     const month = monthOfTheYear( new Date('3/11/2020') );
-    expect( month ).toBe('November');
+    expect( month ).toBe('March');
 })
